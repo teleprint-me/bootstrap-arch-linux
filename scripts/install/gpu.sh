25f830119d02255d6a0625892672bc98bba7d5c9
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-06-13 19:07:53 -0400

    refactor: Move python functions from gpu.sh to python.sh
    - Refactor gpu.sh script to handle GPU core packages
    - Refactor python.sh script to handle python packages
    - Source python.sh from gpu.sh to handle GPU specific architectures
    - MLAI libraries are intelligently handled when installing GPU drivers
    - Gaming libraries are intelligently handled when installing GPU drivers
    - OpenGL and OpenCL will be available by default after installation

--- scripts/install/gpu.sh
+++ scripts/install/gpu.sh
@@ -1,24 +1,41 @@
 #!/usr/bin/env bash
 
+source ./scripts/install/python.sh
+
 # Function to install OpenCL
-setup_opencl() {
+install_opencl() {
     if ! sudo pacman -S openblas openblas64 opencl-headers libclc opencl-clhpp ocl-icd lib32-ocl-icd miopengemm clinfo clpeak nvtop --noconfirm; then
         echo "Failed to install OpenCL"
         exit 1
     fi
 }
 
+install_intel_vulkan() {
+    if ! sudo pacman -S vulkan-intel lib32-vulkan-intel vulkan-icd-loader lib32-vulkan-icd-loader vkd3d lib32-vkd3d vulkan-headers vulkan-validation-layers vulkan-tools --noconfirm; then
+        echo "Failed to install Intel Vulkan driver support"
+        exit 1
+    fi
+}
+
 # Function to install Intel OpenCL
-setup_intel_opencl() {
+install_intel_opencl() {
     if ! sudo pacman -S intel-graphics-compiler intel-compute-runtime intel-opencl-clang --noconfirm; then
         echo "Failed to install Intel OpenCL"
         exit 1
     fi
 }
 
+install_nvidia_vulkan() {
+    # TODO
+    if ! sudo pacman -S nvidia nvidia-settings nvidia-utils lib32-nvidia-utils vulkan-icd-loader lib32-vulkan-icd-loader vkd3d lib32-vkd3d vulkan-headers vulkan-validation-layers vulkan-tools --noconfirm; then
+        echo "Failed to install Nvidia Vulkan driver support"
+        exit 1
+    fi
+}
+
 # Function to install Nvidia CUDA
-setup_nvidia_cuda() {
-    if ! sudo pacman -S nvidia nvidia-settings nvidia-utils nccl opencl-nvidia lib32-opencl-nvidia cuda cudnn cuda-tools opencv-cuda tensorflow-cuda tensorflow-opt-cuda python-cuda python-cuda-docs python-pytorch-cuda python-pytorch-opt-cuda python-tensorflow-cuda python-tensorflow-opt-cuda python-torchvision-cuda --noconfirm; then
+install_nvidia_cuda() {
+    if ! sudo pacman -S nccl opencl-nvidia lib32-opencl-nvidia cuda cudnn cuda-tools opencv-cuda tensorflow-cuda tensorflow-opt-cuda python-cuda python-cuda-docs python-pytorch-cuda python-pytorch-opt-cuda python-tensorflow-cuda python-tensorflow-opt-cuda python-torchvision-cuda --noconfirm; then
         echo "Failed to install Nvidia CUDA"
         exit 1
     fi
@@ -26,7 +43,7 @@ setup_nvidia_cuda() {
 
 # Function to install AMD Vulkan driver support
 install_amd_vulkan() {
-    if ! sudo pacman -S mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon vulkan-icd-loader lib32-vulkan-icd-loader vkd3d lib32-vkd3d -noconfirm; then
+    if ! sudo pacman -S mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon vulkan-icd-loader lib32-vulkan-icd-loader vkd3d lib32-vkd3d vulkan-headers vulkan-validation-layers vulkan-tools --noconfirm; then
         echo "Failed to install AMD Vulkan driver support"
         exit 1
     fi
@@ -34,17 +51,32 @@ install_amd_vulkan() {
 }
 
 # Function to install AMD ROCm
-setup_amd_rocm() {
-    if ! sudo pacman -S mesa opencl-mesa lib32-opencl-mesa rocm-core rocm-llvm rocm-clang-ocl rocm-cmake rocm-smi-lib rocm-hip-libraries rocm-hip-runtime rocm-hip-sdk rocm-language-runtime rocm-opencl-runtime rocm-opencl-sdk rocm-device-libs rocm-ml-libraries rocm-ml-sdk rocminfo hipblas rocblas rocsparse python-pytorch-rocm python-pytorch-opt-rocm --noconfirm; then
+install_amd_rocm() {
+    if ! sudo pacman -S opencl-mesa lib32-opencl-mesa rocm-core rocm-llvm rocm-clang-ocl rocm-cmake rocm-smi-lib rocm-hip-libraries rocm-hip-runtime rocm-hip-sdk rocm-language-runtime rocm-opencl-runtime rocm-opencl-sdk rocm-device-libs rocm-ml-libraries rocm-ml-sdk rocminfo hipblas rocblas rocsparse python-pytorch-rocm python-pytorch-opt-rocm --noconfirm; then
         echo "Failed to install AMD ROCm"
         exit 1
     fi
 }
 
-# Function to install Python machine learning and AI libraries
-setup_python_mlai() {
-    if ! yay -S pyopencl-headers python-pyopencl python-numpy python-scikit-learn python-sentencepiece python-sentence-transformers python-transformers --noconfirm; then
-        echo "Failed to install Python machine learning and AI libraries"
-        exit 1
-    fi
+install_intel() {
+    install_opencl
+    install_intel_opencl
+    install_intel_vulkan
+    install_python_mlai
+}
+
+install_nvidia() {
+    install_opencl
+    install_nvidia_vulkan
+    install_nvidia_cuda
+    install_python_mlai
+    install_python_pytorch_cuda_extension
+}
+
+install_amd() {
+    install_opencl
+    install_amd_vulkan
+    install_amd_rocm
+    install_python_mlai
+    install_python_pytorch_rocm_extension
 }
--- scripts/install/python.sh
+++ scripts/install/python.sh
@@ -2,10 +2,39 @@
 
 # Function to install Python development tools
 install_python() {
-    if ! sudo pacman -S python python-pip python-venv python-virtualenv python-black flake8 mypy -noconfirm; then
+    if ! sudo pacman -S python-pip python-pytest python-pipx python-virtualenv python-isort python-black flake8 ruff mypy --noconfirm; then
         echo "Failed to install python development packages"
         exit 1
     fi
-    pip install --user --upgrade pipx
-    pipx install poetry
+}
+
+# Function to install Python machine learning and AI libraries
+install_python_mlai_dependencies() {
+    if ! yay -S pyopencl-headers python-pyopencl python-numpy python-matplotlib python-nltk python-scikit-learn python-sentencepiece python-sentence-transformers python-transformers python-huggingface-hub --noconfirm; then
+        echo "Failed to install Python machine learning and AI libraries"
+        exit 1
+    fi
+}
+
+install_python_mlai_extensions() {
+    # Install python dependencies
+    pip install --upgrade dotenv dateutil pytz iso8601 requests pdfminer-six chromadb tiktoken langchain
+
+    # Install dev dependencies
+    pip install --upgrade bpython mkdocs requests-mock
+}
+
+install_python_pytorch_cuda_extension() {
+    # Install bitsandbytes
+    pip install --upgrade bitsandbytes
+}
+
+install_python_pytorch_rocm_extension() {
+    # Install bitsandbytes with ROCm support
+    pip install --upgrade "git+https://github.com/broncotc/bitsandbytes-rocm@1b52f4243f94cd1b81dd1cad5a9465d9d7add858"
+}
+
+install_python_mlai() {
+    install_python_mlai_dependencies
+    install_python_mlai_extensions
 }

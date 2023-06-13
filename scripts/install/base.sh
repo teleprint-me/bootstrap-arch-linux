5f923420e9ff0cd2704208ac0a38fbac4adff13b
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-06-13 17:36:05 -0400

    refactor: Update comment, remove flatpak, and add rust
    Signed-off-by: teleprint-me <77757836+teleprint-me@users.noreply.github.com>

--- scripts/install/base.sh
+++ scripts/install/base.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-# function to install Base, C, and C++ development tools
+# Function to install C, C++, Rust, Lua, and Base development tools
 install_base() {
-    if ! sudo pacman -S base-devel llvm clang gdb make cmake ninja lua aspell hunspell shellcheck mlocate nmap curl wget openssl openssh gnupg imagemagick ffmpegthumbs ffmpegthumbnailer flatpak -y; then
+    if ! sudo pacman -S base-devel llvm clang rust gdb make cmake ninja lua aspell hunspell shellcheck mlocate nmap curl wget openssl openssh gnupg imagemagick ffmpegthumbs ffmpegthumbnailer -y; then
         echo "Failed to install core system packages"
         exit 1
     fi

d44269fa759a0deaa6f2d516dcd0063aab8e771a
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-06-13 19:17:31 -0400

    refactor: Add tree to base install
    Signed-off-by: teleprint-me <77757836+teleprint-me@users.noreply.github.com>

--- scripts/install/base.sh
+++ scripts/install/base.sh
@@ -2,7 +2,7 @@
 
 # Function to install C, C++, Rust, Lua, and Base development tools
 install_base() {
-    if ! sudo pacman -S base-devel llvm clang rust gdb make cmake ninja lua aspell hunspell shellcheck mlocate nmap curl wget openssl openssh gnupg imagemagick ffmpegthumbs ffmpegthumbnailer -y; then
+    if ! sudo pacman -S base-devel llvm clang rust gdb make cmake ninja lua aspell hunspell shellcheck mlocate nmap curl wget openssl openssh gnupg imagemagick ffmpegthumbs ffmpegthumbnailer tree -y; then
         echo "Failed to install core system packages"
         exit 1
     fi

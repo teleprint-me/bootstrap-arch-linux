1ae8c7a8ab1788229ef1c0f18ca52e95b86b7064
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-06-13 19:19:43 -0400

    refactor: Decouple installing gnome-desktop from gnome-shell-extension*
    Signed-off-by: teleprint-me <77757836+teleprint-me@users.noreply.github.com>

--- scripts/install/gnome.sh
+++ scripts/install/gnome.sh
@@ -1,8 +1,20 @@
 #!/usr/bin/env bash
 
-function setup_gnome() {
-    if ! yay -S gnome-desktop gnome-shell-extensions gnome-browser-connector gnome-shell-extension-arc-menu gnome-shell-extension-appindicator gnome-shell-extension-material-shell gnome-tweaks gnome-terminal-transparency --noconfirm; then
+function install_gnome_desktop() {
+    if ! pacman -S gnome-desktop --noconfirm; then
         echo "Failed to install Gnome desktop environment"
         exit 1
     fi
+}
+
+function install_gnome_extensions() {
+    if ! yay -S gnome-shell-extensions gnome-browser-connector gnome-shell-extension-arc-menu gnome-shell-extension-appindicator gnome-shell-extension-material-shell gnome-tweaks gnome-terminal-transparency --noconfirm; then
+        echo "Failed to install Gnome shell extensions"
+        exit 1
+    fi
+}
+
+function install_gnome() {
+    install_gnome_desktop
+    install_gnome_extensions
 }

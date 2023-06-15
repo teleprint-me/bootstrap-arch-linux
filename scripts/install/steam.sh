b282d386574cb64dbc9dbdd72ed650f99f66aae3
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-06-14 20:00:42 -0400

    patch: Move, and rename function, from setup to install, Remove sudo from yay call, Update steam source
    Signed-off-by: teleprint-me <77757836+teleprint-me@users.noreply.github.com>

--- scripts/install/steam.sh
+++ scripts/install/steam.sh
@@ -3,14 +3,14 @@
 source ./scripts/tools/confirm.sh
 
 # Function to install steam
-setup_steam() {
+install_steam() {
     confirm_proceed "Install Steam and Steam Play dependencies" || return
 
-    if ! sudo yay -S steam steam-native-runtime wine winetricks protontricks-git proton-ge-custom-bin xboxdrv --noconfirm; then
+    if ! yay -S steam steam-native-runtime wine winetricks protontricks-git proton-ge-custom-bin xboxdrv --noconfirm; then
         echo "Failed to install Steam"
         return 1
     fi
-    
+
     if ! sudo systemctl enable xboxdrv; then
         echo "Failed to enable xbox driver support"
         return 1

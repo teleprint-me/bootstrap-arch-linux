08a040f749493f214ee2720ce26d855b2c46ffdc
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-06-20 16:02:49 -0400

    Add gnome-terminal install and gnome-console removal
    - Add function to install Gnome Terminal from the AUR
    - Set Gnome Terminal as the default terminal emulator
    - Remove Gnome Console package
    - Ask for user confirmation before installation and removal
    - Add error handling to exit script if any operation fails

--- scripts/install/gnome.sh
+++ scripts/install/gnome.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
+# This script installs the Gnome Desktop Environment, several Gnome shell extensions, and the Gnome Terminal with transparency feature from the AUR.
+# It asks for user confirmation before installing each component and before removing the Gnome Console.
+# If any operation fails, the script informs the user and exits with an error status.
+# On successful completion, the script sets Gnome Terminal as the default terminal emulator and removes Gnome Console.
+
 source ./scripts/tools/confirm.sh
 
-function install_gnome_desktop() {
+install_gnome_desktop() {
     confirm_proceed "Gnome Desktop Environment" || return
 
     if ! sudo pacman -S gnome-desktop --noconfirm; then
@@ -11,16 +16,40 @@ function install_gnome_desktop() {
     fi
 }
 
-function install_gnome_extensions() {
+install_gnome_extensions() {
     confirm_proceed "Gnome Shell Extensions" || return
 
-    if ! yay -S gnome-shell-extensions gnome-browser-connector gnome-shell-extension-arc-menu gnome-shell-extension-appindicator gnome-shell-extension-material-shell gnome-tweaks gnome-terminal-transparency --noconfirm; then
+    if ! yay -S gnome-shell-extensions gnome-browser-connector gnome-shell-extension-arc-menu gnome-shell-extension-appindicator gnome-shell-extension-material-shell gnome-tweaks --noconfirm; then
         echo "Failed to install Gnome shell extensions"
         exit 1
     fi
 }
 
-function install_gnome() {
+install_gnome_terminal() {
+    confirm_proceed "Gnome Terminal Transparency (AUR)" || return
+
+    if ! yay -S gnome-terminal-transparency --noconfirm; then
+        echo "Failed to install gnome-terminal-transparency"
+        exit 1
+    fi
+
+    confirm_proceed "Gnome Console" "remove" || return
+
+    # This replaces `kgx` with `gnome-terminal` as the default
+    if ! gsettings set org.gnome.desktop.default-applications.terminal exec /usr/bin/gnome-terminal; then
+        echo "Failed to set gnome-terminal as the default"
+        exit 1
+    fi
+
+    # This removes `kgx` from the gnome environment
+    if ! sudo pacman -R gnome-console --noconfirm; then
+        echo "Failed to remove gnome-console"
+        exit 1
+    fi
+}
+
+install_gnome() {
     install_gnome_desktop
     install_gnome_extensions
+    install_gnome_terminal
 }

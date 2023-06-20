a849030a35bf98ee18fc0202443769aae80a412f
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-06-20 16:04:15 -0400

    Enhance confirm_proceed function in confirm.sh
    - Add optional argument to confirm_proceed function for removal confirmation
    - Improve function flexibility to handle both installation and removal scenarios

--- scripts/tools/confirm.sh
+++ scripts/tools/confirm.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
-# Function to confirm before proceeding with installation
+# Function to confirm before proceeding with installation or removal
 confirm_proceed() {
-    read -p "This will install $1 and may take some time. Are you sure? (y/n) " -n 1 -r
+    local operation="install"
+    if [[ $2 == "remove" ]]; then
+        operation="remove"
+    fi
+
+    read -p "This will ${operation} $1 and may take some time. Are you sure? (y/n) " -n 1 -r
     echo    # move to a new line
     if [[ ! $REPLY =~ ^[Yy]$ ]]
     then
-        echo "Installation of $1 cancelled."
+        echo "${operation^}ation of $1 cancelled."
         return 1
     fi
 }

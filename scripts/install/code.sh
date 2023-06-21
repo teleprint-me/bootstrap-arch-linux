2550ae2738b325dd96185af6f971f4b228616cf3
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-06-20 20:54:21 -0400

    Handle mime-types during VSCode installHandle mime-types during VSCode install
    - Added check for xdg-mime command existence before resetting mime types
    - Reset mime types after installing VSCode
    - Updated comments for clarity and future-proofing
    - Factored out mime type resetting to its own functions

--- scripts/install/code.sh
+++ scripts/install/code.sh
@@ -2,7 +2,72 @@
 
 source ./scripts/tools/confirm.sh
 
-# Function to install Python development tools
+# Developer Notes: 
+
+# VSCode does NOT respect the users environmental mime-types and modifies them.
+
+# This has undesirable and adverse side-effects as a result.
+
+# Functions are included here to mitigate these issues.
+
+# TODO: Come up with a better way of handling variable environments along with mime types for differing environments.
+# Handling the defaults like this is fine for now, but will become an annoyance in the future.
+
+# NOTE: Modify these env variables to suite your environment or clear them to enable prompting.
+# DEFAULT_FILE_MANAGER="org.kde.dolphin.desktop" for dolphin on KDE
+DEFAULT_FILE_MANAGER="org.gnome.Nautilus.desktop"
+# NOTE: A lighter weight text editor is preferred in most cases which is why vscode is overridden.
+# DEFAULT_TEXT_EDITOR="kate.desktop" for KDE
+# DEFAULT_TEXT_EDITOR="code.desktop" for VSCode
+DEFAULT_TEXT_EDITOR="nvim.desktop"
+
+reset_inode_mime_types() {
+    local default_mime_type
+
+    if [ -z "$DEFAULT_FILE_MANAGER" ]; then
+        echo "Please enter the name of your preferred file manager:"
+        read -r DEFAULT_FILE_MANAGER
+    fi
+
+    default_mime_type="$(xdg-mime query filetype ~)"
+
+    if ! xdg-mime default "${DEFAULT_FILE_MANAGER}" "${default_mime_type}"; then
+        echo "Failed to reset default MIME type for directories"
+        return 1
+    fi
+
+    echo "XDG Inode MIME types successfully restored"
+}
+
+reset_text_mime_types() {
+    local default_mime_type
+
+    if [ -z "$DEFAULT_TEXT_EDITOR" ]; then
+        echo "Please enter the name of your preferred text editor:"
+        read -r DEFAULT_TEXT_EDITOR
+    fi
+
+    default_mime_type="$(xdg-mime query filetype ~/.bashrc)"
+
+    if ! xdg-mime default "${DEFAULT_TEXT_EDITOR}" "${default_mime_type}"; then
+        echo "Failed to set ${DEFAULT_TEXT_EDITOR} as the default text editor"
+        return 1
+    fi
+
+    echo "XDG Text MIME types successfully restored"
+}
+
+reset_vscode_mime_types() {
+    # Check if xdg-mime exists
+    if ! command -v xdg-mime &> /dev/null; then
+        echo "xdg-mime could not be found. Aborting."
+        return 1
+    fi
+
+    reset_inode_mime_types
+    reset_text_mime_types
+}
+
 install_vscode() {
     confirm_proceed "Visual Studio Code" || return
 
@@ -10,4 +75,8 @@ install_vscode() {
         echo "Failed to install Visual Studio Code"
         return 1
     fi
+
+    reset_vscode_mime_types
+
+    echo "Visual Studio Code installed successfully"
 }
